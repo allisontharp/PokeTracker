c1f99f9c6df960998d6d3f61fbd593818cddd475
allisontharp <junk+github@techtrek.io>
2020-03-09 16:45:49 -0400

    caught status working

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { PokemondbService } from '../pokemondb.service';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -9,9 +10,18 @@ export class PokemonCardComponent implements OnInit {
   @Input()
   pokemon: any;
   
-  constructor() { }
+  constructor(
+    public pokemonDb: PokemondbService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  async updateSinglePokemon(pokemonNumber, region, isCaught, isFavorite){
+    console.log(`favoriteicon.updateSinglePokemon(${pokemonNumber}, ${region}, ${isCaught}, ${isFavorite}) called.`);
+    this.pokemon.favorite = isFavorite;
+
+    await this.pokemonDb.updateSinglePokemon(pokemonNumber, region, isCaught, isFavorite);
   }
 
 }
