75cdc910ea782c4fbbee7b8d0ac696d35138f2a0
allisontharp <junk+github@techtrek.io>
2020-03-17 18:00:28 -0400

    MVP working

--- src/app/pokemon-details/pokemon-details.component.ts
+++ src/app/pokemon-details/pokemon-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { PokemondbService } from '../pokemondb.service';
 import allPokemon from "../../assets/pokemon.json"
+import allTypes from "../../assets/types.json"
 
 @Component({
   selector: 'app-pokemon-details',
@@ -12,12 +12,13 @@ export class PokemonDetailsComponent implements OnInit {
   private sub: any;
   GameName: string;
   numberNational;
-  pokemon;
+  pokemonName;
+  monTypes: any;
+  monMultipliers: any;
 
 
   constructor(
-    private route: ActivatedRoute,
-    private pokemonDb: PokemondbService
+    private route: ActivatedRoute
   ) { }
 
   async ngOnInit(): Promise<void> {
@@ -26,7 +27,73 @@ export class PokemonDetailsComponent implements OnInit {
       this.numberNational = params['numberNational'];
     });
 
-    var mon = allPokemon.filter(i=>i.number == this.numberNational);
+    this.monTypes = new Array;
+
+    var mon = allPokemon.filter(i=>i.number == this.numberNational)[0];
+    this.pokemonName = mon.name;
+
+    mon.types.forEach(t => {
+      var typeDetails = allTypes.filter(d => d.name == t.type.name)[0]
+      var details = typeDetails;
+      details['slot'] = t.slot
+      this.monTypes.push(details);
+    });
+
+    this.monMultipliers = this.getWeaknesses();
+    console.log(this.monMultipliers);
+    
   }
 
+  getWeaknesses(){
+    var typeMultiplier = new Array;
+    this.monTypes.forEach(typeName => {
+      var slot = typeName.slot;
+      allTypes.forEach(type => {
+        var multiplier = 1;
+        if(type.strengths.indexOf(typeName.name) >=0){ 
+          multiplier = 2
+        } else if(type.weaknesses.indexOf(typeName.name) >=0) { 
+          multiplier = 0.5
+        } else if(type.immunes.indexOf(typeName.name) >=0){
+          multiplier = 0
+        }
+
+        var currentType = typeMultiplier.filter(t => t.name == type.name)
+        if(currentType.length == 0)
+        {
+          typeMultiplier.push({"name": type.name, "multiplier": multiplier,})
+        }
+        else{
+          currentType[0]['multiplier'] = currentType[0]['multiplier'] * multiplier;
+        }
+        var category;
+        currentType = typeMultiplier.filter(t => t.name == type.name)
+
+        if(currentType[0]['multiplier'] == 0){
+          category = 'immune'
+        }else if (currentType[0]['multiplier'] == 1){
+          category = 'normal'
+        }
+        else if (currentType[0]['multiplier'] > 1){
+          category = 'weak'
+        }else if(currentType[0]['multiplier'] < 1){
+          category = 'resistant'
+        }
+        currentType[0]['category'] = category;
+      });
+    });
+
+    // console.log('Damaged Normally By:')
+    // console.log(typeMultiplier.filter(f => f.multiplier == 1))
+    // console.log('Weak to:')
+    // console.log(typeMultiplier.filter(f => f.multiplier > 1))
+
+    // console.log('Immune to:')
+    // console.log(typeMultiplier.filter(f => f.multiplier == 0))
+
+    // console.log('Resistant to:')
+    // console.log(typeMultiplier.filter(f => f.multiplier < 1))
+
+    return typeMultiplier;
+  }
 }
